c728d27d5683244c63409681e71d9e371e06abf1
Kiyoto Sato <k-sato@du.jp.nec.com>
2019-05-29 14:36:27 +0900

    test

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,14 @@
 from flask import Flask
 application = Flask(__name__)
 
+@application.route("/log")
+def log():
+    print("TEST LOG")
+    return "Test Log"
+
 @application.route("/")
 def hello():
-    return "Hello World 2 !"
+    return "Hello World !"
 
 if __name__ == "__main__":
     application.run()
